945d25421e189cb58e2f12fd7a3aba9df05b4c9e
toneDrei79 <tone0709@icloud.com>
2023-03-31 18:45:21 +0200

    manage files

---
+++ ex01/mp/anaglyph.cu
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <cfloat>
+#include <opencv2/core/cuda/common.hpp>
+#include <opencv2/core/cuda/border_interpolate.hpp>
+#include <opencv2/core/cuda/vec_traits.hpp>
+#include <opencv2/core/cuda/vec_math.hpp>
+
+#include "helper_math.h"
+
+
+__global__ void process(const cv::cuda::PtrStep<uchar3> src, cv::cuda::PtrStep<uchar3> dst, int rows, int cols, float* mat_l, float* mat_r)
+{
+    const int dst_x = blockDim.x * blockIdx.x + threadIdx.x;
+    const int dst_y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    uchar3 left = src(dst_y, dst_x);
+    uchar3 right = src(dst_y, dst_x + cols);
+
+    // GBR order
+    dst(dst_y, dst_x).z = char(mat_l[0]*left.z + mat_l[1]*left.y + mat_l[2]*left.x + mat_r[0]*right.z + mat_r[1]*right.y + mat_r[2]*right.x);
+    dst(dst_y, dst_x).y = char(mat_l[3]*left.z + mat_l[4]*left.y + mat_l[5]*left.x + mat_r[3]*right.z + mat_r[4]*right.y + mat_r[5]*right.x);
+    dst(dst_y, dst_x).x = char(mat_l[6]*left.z + mat_l[7]*left.y + mat_l[8]*left.x + mat_r[6]*right.z + mat_r[7]*right.y + mat_r[8]*right.x);
+}
+
+int divUp(int a, int b)
+{
+    return ((a % b) != 0) ? (a / b + 1) : (a / b);
+}
+
+// receive matrices as 1-dim pointer
+void startCUDA(cv::cuda::GpuMat& src, cv::cuda::GpuMat& dst, float* mat_l, float* mat_r)
+{
+    const dim3 block(32, 8);
+    const dim3 grid(divUp(dst.cols, block.x), divUp(dst.rows, block.y));
+
+    // matrices on device
+    float* dmat_l;
+    float* dmat_r;
+
+    // allocate memory on device
+    size_t mat_size;
+    mat_size = sizeof(float) * 9;
+    cudaMalloc((void **)&dmat_l, mat_size);
+    cudaMalloc((void **)&dmat_r, mat_size);
+    cudaMemcpy(dmat_l, mat_l, mat_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dmat_r, mat_r, mat_size, cudaMemcpyHostToDevice);
+
+    process<<<grid, block>>>(src, dst, dst.rows, dst.cols, dmat_l, dmat_r);
+
+    cudaFree(dmat_l);
+    cudaFree(dmat_r);
+}
---
+++ ex01/mp/anaglyphs.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+void set_true_mats(float matL[3][3], float matR[3][3]);
+
+void set_gray_mats(float matL[3][3], float matR[3][3]);
+
+void set_color_mats(float matL[3][3], float matR[3][3]);
+
+void set_halfcolor_mats(float matL[3][3], float matR[3][3]);
+
+void set_optimized_mats(float matL[3][3], float matR[3][3]);
